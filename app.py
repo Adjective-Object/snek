187cade9c837b401b0d32f71a58060a21a950c75
Maxwell Huang-Hobbs <mhuan13@gmail.com>
2016-07-09 22:43:46 -0400

    removed site subdir (just going to be static)

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from config import config
-from api.routes import apply_routes as apply_api_routes
+from api.routes import blueprint as api_blueprint
 
+# init application
 app = Flask(__name__)
-apply_api_routes(app)
 
+# register api blueprints
+app.register_blueprint(api_blueprint, url_prefix='/api') # api
+
+# Routes for serving static files
+@app.route('/', methods=['GET'])
+def send_static_site():
+    return send_from_directory('site', 'index.html')
+
+@app.route('/cache/<path:path>', methods=['GET'])
+def send_from_cache(path):
+    return send_from_directory(config.paths.cache, path)
+
+# run the app
 app.run(**config.app_config)
 
