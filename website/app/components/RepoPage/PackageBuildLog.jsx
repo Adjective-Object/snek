59ba62819ea6e692dc49eee21fa9491434809a7b
Maxwell Huang-Hobbs <mhuan13@gmail.com>
2016-07-18 22:30:53 -0400

    added basic logs display

--- website/app/components/RepoPage/PackageBuildLog.jsx
+++ website/app/components/RepoPage/PackageBuildLog.jsx
@@ -2,11 +2,22 @@ import React, {Component} from 'react';
 
 export default class PackageBuildLog extends Component {
   render() {
-    return (<pre>
-      {JSON.stringify(this.props.buildLog, null, 2)
-        .replace(/\\n/g, '\n        ')
-      }
-    </pre>);
+    let steps = [];
+    for (let step in this.props.buildLog) {
+      steps.push(
+        <div className='build-log-step' key={ step }>
+          <h3>{ step }</h3>
+          <pre>
+            { this.props.buildLog[step] }
+          </pre>
+        </div>
+        )
+    }
+    return (
+      <div>
+        { steps }
+      </div>
+    );
   }
 }
 PackageBuildLog.propTypes = {
